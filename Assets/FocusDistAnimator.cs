52ca38d73e2c3ca42b8d28323dc84d718ed1d18b
Oleg Frolov <frolololeg@gmail.com>
2020-11-11 22:12:38 +0300

    Added PostProcessing FX

---
+++ Assets/FocusDistAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class FocusDistAnimator : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume postProcessVolume;
+    private DepthOfField _depthOfField;
+    private float _focusDistance = 10f;
+
+    private void Start()
+    {
+        postProcessVolume.profile.TryGetSettings(out _depthOfField);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_focusDistance <= 1f) return;
+        _focusDistance -= Time.deltaTime * 0.25f;
+        _depthOfField.focusDistance.value = _focusDistance;
+    }
+}
